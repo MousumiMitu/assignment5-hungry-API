2d53694667ffe780a9addb1eb82ac201b0cea3dc
MousumiMitu <mousumitu@gmail.com>
2021-02-07 20:25:58 +0600

    ingredients part

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const getMeal = (food) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`;
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${food}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => getMealLists(data.meals));
@@ -12,15 +12,37 @@ const getFoodInfo = () => {
 
 const getMealLists = (foodKeywords) => {
   const mealArea = document.getElementById("food-area");
-  foodKeywords.forEach((user) => {
+  foodKeywords.forEach((meal) => {
     const foodDiv = document.createElement("div");
     foodDiv.className = "meal-container";
     const foodData = `
-        <img src="${user.strMealThumb}">
-        <h4> ${user.strMeal}</h4>
+        <img onclick="getBtn('${meal.strMeal}')" src="${meal.strMealThumb}">
+        <h4 onclick="getBtn('${meal.strMeal}')"> ${meal.strMeal}</h4>
     `;
     foodDiv.innerHTML = foodData;
     mealArea.appendChild(foodDiv);
-    document.getElementById("food-input").value = "";
   });
+
+  document.getElementById("food-input").value = "";
+};
+const getBtn = (food) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => getIngredientList(data.meals[0]));
+};
+const getIngredientList = (ingredient) => {
+  const listDiv = document.getElementById("ingredient-list");
+  listDiv.innerHTML = `
+  <img src="${ingredient.strMealThumb}">
+  <h3>${ingredient.strIngredient1}</h3>
+  <h3>${ingredient.strIngredient2}</h3>
+  <h3>${ingredient.strIngredient3}</h3>
+  <h3>${ingredient.strIngredient4}</h3>
+  <h3>${ingredient.strIngredient5}</h3>
+  <h3>${ingredient.strIngredient6}</h3>
+  <h3>${ingredient.strIngredient7}</h3>
+  <h3>${ingredient.strIngredient8}</h3>
+  <h3>${ingredient.strIngredient9}</h3>
+  `;
 };
